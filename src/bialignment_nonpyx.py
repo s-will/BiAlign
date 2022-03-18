f105bc8bcad345463f096ebf39f8c0921d85aeb1
Sebastian Will <swill@csail.mit.edu>
2022-03-18 15:56:14 +0100

    Bump version

--- src/bialignment_nonpyx.py
+++ src/bialignment_nonpyx.py
@@ -1,6 +1,6 @@
 from collections import defaultdict
 
-__version__ = "0.3b4"
+__version__ = "0.3b5"
 
 blosum62 = """-  A  R  N  D  C  Q  E  G  H  I  L  K  M  F  P  S  T  W  Y  V  B  Z  X  *
 A  4 -1 -2 -2  0 -1 -1  0 -2 -1 -1 -1 -1 -2 -1  1  0 -3 -2  0 -2 -1  0 -4
